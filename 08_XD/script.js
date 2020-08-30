609c67a4a206f2ef04cf6537fd4c13176333cb7f
RobeSantoro <santoro.robe@gmail.com>
2020-08-31 01:50:41 +0200

    Back to Animate.css, V2 Almost ready

--- 08_XD/script.js
+++ 08_XD/script.js
@@ -1,4 +1,4 @@
-function scaleBetween(v, newMin, newMax, oldMin, oldMax) {
+function mapRange(v, newMin, newMax, oldMin, oldMax) {
     /**
      * Returns a scaled number within its source bounds to the desired target bounds.
      * @param {number} v - Unscaled number
@@ -7,10 +7,13 @@ function scaleBetween(v, newMin, newMax, oldMin, oldMax) {
      * @param {number} oldMin - Minimum (source) bound to scale from
      * @param {number} oldMax - Maximum (source) bound to scale from
      */
-    return ((v - oldMin) / (oldMax - oldMin)) * (newMax - newMin) + newMin;
+    let result = ((v - oldMin) / (oldMax - oldMin)) * (newMax - newMin) + newMin;
+    return result.toFixed(2);
 }
 
-const body = document.querySelector('body');
+/* SVG */
+const head = document.querySelector("#head");
+const head_shape = document.querySelector("#head_shape");
 
 const eyes = document.querySelectorAll(".eye");
 const eyes_base = document.querySelector("#eyes_base");
@@ -18,15 +21,15 @@ const eyes_inside = document.querySelector("#eyes_inside");
 
 const mouth_grp = document.querySelector("#mouth_grp");
 const mouth = document.querySelector("#mouth");
-
-const head = document.querySelector("#head");
-const head_shape = document.querySelector("#head_shape");
+const nose_line = document.querySelector("#nose_line");
+const teeth = document.querySelector("#teeth");
 
 const ears = document.querySelector('#ears');
 
+/* DOM */
+const body = document.querySelector('body');
 const inputEmail = document.getElementById('Email');
 const inputPassword = document.getElementById('Password');
-
 const LoginButton = document.querySelector('#LoginButton')
 
 
@@ -79,8 +82,9 @@ else LoginButton.addEventListener('mouseout', mouseOutLogin)
 
 function onLoad() {
     // Everything has loaded!
-    head.classList.add( 'animate__animated' ,'animate__repeat-1', 'animate__bounceInUp', 'animate__delay-1s' );
-    hands.classList.add('animate__animated' ,'animate__repeat-1', 'animate__bounceInUp');
+    head.classList.add('animate__animated', 'animate__repeat-1', 'animate__bounceInUp', 'animate__delay-1s');
+    hands.classList.add('animate__animated', 'animate__repeat-1', 'animate__bounceInUp');
+    return
 
 }
 
@@ -102,8 +106,8 @@ function eyesOnMouse() {
     let oldMinY = 0;
     let oldMaxY = window.innerHeight;
 
-    let x = scaleBetween(vX, newMinX, newMaxX, oldMinX, oldMaxX);
-    let y = scaleBetween(vY, newMinY, newMaxY, oldMinY, oldMaxY);
+    let x = mapRange(vX, newMinX, newMaxX, oldMinX, oldMaxX);
+    let y = mapRange(vY, newMinY, newMaxY, oldMinY, oldMaxY);
 
     eyes[0].setAttribute("transform", `translate (${x} ${y})`);
     eyes[1].setAttribute("transform", `translate (${x} ${y})`);
@@ -116,15 +120,24 @@ function eyesOnMouse() {
     head_shape.setAttribute("transform", `translate (${x / 4} ${y / 4})`);
 
     ears.setAttribute("transform", `translate (${(x / 3) * -1} ${(y / 3) * -1})`);
+    
+    return
 
 }
 
 function eyesOnMail() {
     console.log("EyesOnMail");
 
+    head.classList.remove('animate__repeat-1', 'animate__bounceInUp', 'animate__delay-1s');
+    head.classList.remove('headOnPassword');
+
+
+
     let inputMailLength = inputEmail.value.length;
 
     if (window.innerWidth < 768) {
+
+        head.classList.add('headOnMail768');
 
         let vX = inputMailLength;
 
@@ -134,10 +147,11 @@ function eyesOnMail() {
         let oldMinX = 0;
         let oldMaxX = 30;
 
-        let x = scaleBetween(vX, newMinX, newMaxX, oldMinX, oldMaxX);
+        let x = mapRange(vX, newMinX, newMaxX, oldMinX, oldMaxX);
         let y = 20;
 
-        if (inputMailLength < 30) {
+        if (inputMailLength < 30) { //avoid wrapping by mapRange
+
             eyes[0].setAttribute("transform", `translate (${x} ${y})`);
             eyes[1].setAttribute("transform", `translate (${x} ${y})`);
 
@@ -150,7 +164,7 @@ function eyesOnMail() {
 
             ears.setAttribute("transform", `translate (${(x / 3) * -1} ${(y / 3) * -1})`);
 
-        } else {
+        } else { //avoid wrapping by mapRange
 
             eyes[0].setAttribute("transform", `translate (${newMaxX} ${y})`);
             eyes[1].setAttribute("transform", `translate (${newMaxX} ${y})`);
@@ -168,7 +182,9 @@ function eyesOnMail() {
 
     }
 
-    else {
+    else { // window.innerWidth > 768
+
+        head.classList.add('headOnMail');
 
         let vX = inputMailLength;
 
@@ -178,10 +194,10 @@ function eyesOnMail() {
         let oldMinX = 0;
         let oldMaxX = 30;
 
-        let x = scaleBetween(vX, newMinX, newMaxX, oldMinX, oldMaxX);
+        let x = mapRange(vX, newMinX, newMaxX, oldMinX, oldMaxX);
         let y = -10;
 
-        if (inputMailLength < 30) {
+        if (inputMailLength < 30) { //avoid wrapping by mapRange
             eyes[0].setAttribute("transform", `translate (${x} ${y})`);
             eyes[1].setAttribute("transform", `translate (${x} ${y})`);
 
@@ -193,9 +209,9 @@ function eyesOnMail() {
             head_shape.setAttribute("transform", `translate (${x / 4} ${y / 4})`);
 
             ears.setAttribute("transform", `translate (${(x / 3) * -1} ${(y / 3) * -1})`);
-            
-        } else {
-            
+
+        } else { //avoid wrapping by mapRange
+
             eyes[0].setAttribute("transform", `translate (${newMaxX} ${y})`);
             eyes[1].setAttribute("transform", `translate (${newMaxX} ${y})`);
 
@@ -218,16 +234,107 @@ function eyesOnMail() {
 function eyesOnPassword() {
     console.log("eyesOnPassword");
 
+    head.classList.remove('headOnMail', 'headOnMail768');
+    head.classList.remove('animate__repeat-1', 'animate__bounceInUp', 'animate__delay-1s');
+
+    head.classList.add('headOnPassword');
+
+    let inputPasswordLength = inputPassword.value.length;
+
+    if (window.innerWidth < 768) {
+
+        let v = inputPasswordLength;
+
+        let newMinX = -15;
+        let newMaxX = 15;
+
+        let oldMinX = 0;
+        let oldMaxX = 30;
+
+        let x = mapRange(v, newMinX, newMaxX, oldMinX, oldMaxX);
+        let y = 10;
+
+        if (inputPasswordLength < 30) { //avoid wrapping by mapRange
+
+            eyes[0].setAttribute("transform", `translate (${x} ${y})`);
+            eyes[1].setAttribute("transform", `translate (${x} ${y})`);
+
+            eyes_base.setAttribute("transform", `translate (${x / 2} ${y / 2})`);
+            eyes_inside.setAttribute("transform", `translate (${x / 2} ${y / 2})`);
+
+            mouth_grp.setAttribute("transform", `translate (${x / 2} ${y / 2})`);
+
+            head_shape.setAttribute("transform", `translate (${x / 4} ${y / 4})`);
+
+            ears.setAttribute("transform", `translate (${(x / 3) * -1} ${(y / 3) * -1})`);
+
+        } else { // window.innerWidth > 768
+
+            eyes[0].setAttribute("transform", `translate (${newMaxX} ${y})`);
+            eyes[1].setAttribute("transform", `translate (${newMaxX} ${y})`);
+
+            eyes_base.setAttribute("transform", `translate (${newMaxX / 2} ${y / 2})`);
+            eyes_inside.setAttribute("transform", `translate (${newMaxX / 2} ${y / 2})`);
+
+            mouth_grp.setAttribute("transform", `translate (${newMaxX / 2} ${y / 2})`);
+
+            head_shape.setAttribute("transform", `translate (${newMaxX / 4} ${y / 4})`);
+
+            ears.setAttribute("transform", `translate (${(newMaxX / 3) * -1} ${(y / 3) * -1})`);
+        }
+    }    
+    else { // window.innerWidth > 768
+
+        let v = inputPasswordLength;
+
+        let newMinX = 8;
+        let newMaxX = 25;
+
+        let oldMinX = 0;
+        let oldMaxX = 30;
+
+        let x = mapRange(v, newMinX, newMaxX, oldMinX, oldMaxX);
+        let y = -5;
+
+        if (inputPasswordLength < 30) { //avoid wrapping by mapRange
+            eyes[0].setAttribute("transform", `translate (${x} ${y})`);
+            eyes[1].setAttribute("transform", `translate (${x} ${y})`);
+
+            eyes_base.setAttribute("transform", `translate (${x / 2} ${y / 2})`);
+            eyes_inside.setAttribute("transform", `translate (${x / 2} ${y / 2})`);
+
+            mouth_grp.setAttribute("transform", `translate (${x / 2} ${y / 2})`);
+
+            head_shape.setAttribute("transform", `translate (${x / 4} ${y / 4})`);
+
+            ears.setAttribute("transform", `translate (${(x / 3) * -1} ${(y / 3) * -1})`);
+
+        } else { //avoid wrapping by mapRange
+
+            eyes[0].setAttribute("transform", `translate (${newMaxX} ${y})`);
+            eyes[1].setAttribute("transform", `translate (${newMaxX} ${y})`);
+
+            eyes_base.setAttribute("transform", `translate (${newMaxX / 2} ${y / 2})`);
+            eyes_inside.setAttribute("transform", `translate (${newMaxX / 2} ${y / 2})`);
+
+            mouth_grp.setAttribute("transform", `translate (${newMaxX / 2} ${y / 2})`);
+
+            head_shape.setAttribute("transform", `translate (${newMaxX / 4} ${y / 4})`);
+
+            ears.setAttribute("transform", `translate (${(newMaxX / 3) * -1} ${(y / 3) * -1})`);
+        }
+
+
+    }
+
+    return
 }
 
 function mouseOverLogin() {
     console.log('mouseOverLogin');
 
-
-    head.classList.remove('animate__bounceInUp');
-    head.classList.remove('animate__repeat-1');
-    head.classList.remove('animate__delay-2s');
-
+    head.classList.remove('headOnMail');
+    head.classList.remove('headOnPassword');
 
     let inputMailLength = inputEmail.value.length;
     let inputMailContainsAt = inputEmail.value.includes('@');
@@ -239,43 +346,52 @@ function mouseOverLogin() {
         head.classList.add('animate__delay-1s');
         head.classList.add('animate__repeat-1');
         head.classList.add('animate__fast');
-        head.classList.add('animate__shakeX');
+        head.classList.add('animate__headShake');
 
-        mouth.style.transform = 'scaleY(-1)';
+        mouth.style = 'transform:translateY(5px); transform:scaleY(-1);';
+        nose_line.setAttribute('y2', '246')
+        teeth.style.transform = 'translateY(-14px)';
 
         inputEmail.style.border = 'solid 2px red';
         inputEmail.classList.add('animate__animated');
-        inputEmail.classList.add('animate__headShake');
-        
+        inputEmail.classList.add('animate__shakeX');
+
     } else if (inputPasswordLength < 8) {
 
-        head.classList.add('animate__delay-1s');
-        head.classList.add('animate__repeat-1');
-        head.classList.add('animate__fast');
-        head.classList.add('animate__shakeX');
+        head.classList.add('animate__headShake');
 
-        eyes[0].classList.add('animate__animated');
-        eyes[1].classList.add('animate__headShake', 'animate__delay-1s');
+        mouth.style = 'transform:translateY(5px); transform:scaleY(-1);';
+        nose_line.setAttribute('y2', '246')
+        teeth.style.transform = 'translateY(-14px)';
 
-        mouth.style.transform = 'scaleY(-1)';
+        inputEmail.style.border = 'solid 2px green';
 
         inputPassword.style.border = 'solid 2px red';
+        inputPassword.classList.remove('animate__headShake');
+
         inputPassword.classList.add('animate__animated');
-        inputPassword.classList.add('animate__headShake');
+        inputPassword.classList.add('animate__shakeX');
     }
     else {
         console.log('Mail OK & PASSWORD OK');
-        
-        head.classList.remove('animate__shakeX');
+
+        head.classList.remove('animate__headShake');
         head.classList.add('animate__tada');
         hands.classList.add('animate__tada');
-        
+
         inputEmail.style.border = 'none';
         inputEmail.classList.remove('animate__animated');
         inputEmail.classList.remove('animate__headShake');
-        
+
+        inputPassword.style.border = 'solid 2px green';
+
         mouth.style.transform = 'scaleY(1)';
+        nose_line.setAttribute('y2', '265')
+        teeth.style.transform = 'translateY(0px)';
+
     }
+
+    return
 
 }
 
@@ -283,15 +399,16 @@ function mouseOutLogin() {
     console.log('mouseOutLogin');
 
 
-    head.classList.remove('animate__shakeX');
+    head.classList.remove('animate__headShake');
     head.classList.remove('animate__tada');
-    hands.classList.remove('animate__tada');
-
-    inputEmail.classList.remove('animate__headShake');
+    
+    inputEmail.classList.remove('animate__shakeX');
     inputPassword.classList.remove('animate__headShake');
 
-
     mouth.style.transform = 'scaleY(1)';
+    nose_line.setAttribute('y2', '265')
+    teeth.style.transform = 'translateY(0px)';
 
+    return
 
 }
